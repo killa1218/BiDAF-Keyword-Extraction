b97fff3b3a931bea52095ba325562d872c1e33a4
Unknown <jaytin1218@hotmail.com>
2017-08-22 09:00:34 +0800

    Debug: Fix bug of loading data

--- basic/read_data.py
+++ basic/read_data.py
@@ -255,7 +255,7 @@ def get_squad_data_filter(config):
             return True
 
         if config.sent_size_th: # filt data items longer than
-            if len(x) > config.sent_size_th:
+            if len(xi[0]) > config.sent_size_th:
                 return False
 
         if config.single:
